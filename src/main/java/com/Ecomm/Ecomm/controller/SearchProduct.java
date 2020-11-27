fa649a3536cdb317e4fe1e30dad2e8df18b36d8c
abhinashsivan <qwerty@00098>
2020-11-27 14:28:44 +0530

    changed requestparam in SearchProduct to pathvaribale

--- src/main/java/com/Ecomm/Ecomm/controller/SearchProduct.java
+++ src/main/java/com/Ecomm/Ecomm/controller/SearchProduct.java
@@ -8,11 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,11 +26,10 @@ public class SearchProduct {
     @Autowired
     private CustomerRepository customerRepository;
 
-    @GetMapping("/product/search")
-    public ResponseEntity<String> searchProduct(@RequestParam Long id) {
+    @GetMapping("/product/search/{id}")
+    public ResponseEntity<String> searchProduct(@PathVariable Long id) {
 
         Optional<Product> product = productService.findProductById(id);
-
 
 
         if (product.isPresent()) {
@@ -42,9 +41,7 @@ public class SearchProduct {
             }
             return new ResponseEntity<>("product name: " + product.get().getProductName().toUpperCase() +
                     "  reviewed users: " + reviewedCusNames.toString().toUpperCase(), HttpStatus.FOUND);
-        }
-
-        else
+        } else
             return new ResponseEntity<>("NO SUCH PRODUCT FOUND", HttpStatus.NOT_FOUND);
     }
 }
